b4978bdc6283a80ea51ee20670fba0c73883ca65
sushrandive <sushrandive@gmail.com>
2020-05-06 19:33:06 +0530

    unwanted imports

--- src/main/java/com/planner/calc/service/PlannerServiceApplication.java
+++ src/main/java/com/planner/calc/service/PlannerServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
+
 
 import com.planner.calc.service.prop.config.AppSql;
 
